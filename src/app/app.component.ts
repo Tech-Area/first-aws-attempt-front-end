df238adeefe57fb808002559ddbcc000372b7186
steffanciprian <steffanciprian@gmail.com>
2022-06-22 21:11:20 +0300

    first commit front end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, NgModule} from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'aws-first-attempt-front-end';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { IonutComponent } from './ionut/ionut.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    IonutComponent
   ],
   imports: [
     BrowserModule
